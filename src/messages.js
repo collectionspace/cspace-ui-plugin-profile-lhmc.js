f285f87a4d727158da61d35c5bb47d2f490ca941
Ray Lee <ray.lee@lyrasis.org>
2019-07-08 17:05:13 -0700

    DRYD-688: Update login page messages.

--- src/messages.js
+++ src/messages.js
@@ -1,4 +1,51 @@
 export default {
+  'about.title': 'Welcome to the CollectionSpace Local History and Material Culture Demo',
+  'about.contentHTML': `
+    <p>
+      CollectionSpace is a free, open-source collections management application for museums,
+      historical societies, natural science collections, and more.
+    </p>
+    <p>
+      This demo site is running the
+      <a href="http://www.collectionspace.org/current_release/">current version</a> of the
+      <a href="https://collectionspace.atlassian.net/wiki/x/BYA_K">local history and material culture profile</a> of
+      CollectionSpace, which includes fields and procedures useful for local history collections. If
+      you’re interested in a version of CollectionSpace configured to meet the needs of a different
+      domain, please visit our <a href="https://www.collectionspace.org/demo/">demo landing page</a>
+      to view all available options. Examples include Anthropology, Botanical Garden, Fine Art,
+      Public Art, and Herbarium.
+    </p>
+    <p>
+      To log in, use the email <strong>admin@lhmc.collectionspace.org</strong>, with the
+      case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
+      email <strong>reader@lhmc.collectionspace.org</strong> with the password
+      <strong>reader</strong>.
+    </p>
+    <p>
+      Not sure where to get started? Follow along with one of our quick-start guides:
+    </p>
+    <ul>
+      <li>
+        <span>Create a new object:</span>
+        <a href="http://bit.ly/newobjpdf">PDF</a>
+        <a href="https://vimeo.com/112212895">Screencast</a>
+      </li>
+      <li>
+        <span>Link to an image:</span>
+        <a href="http://bit.ly/linkimgpdf">PDF</a>
+        <a href="https://vimeo.com/112214418">Screencast</a>
+      </li>
+      <li>
+        <span>Add a storage location:</span>
+        <a href="http://bit.ly/storlocpdf">PDF</a>
+        <a href="https://vimeo.com/112818120">Screencast</a>
+      </li>
+    </ul>
+    <p>
+      We rebuild this demo site weekly so don't worry, you won't break it!
+    </p>
+  `,
+
   'field.acquisitions_common.accessionDateGroup.name': 'Accessioned',
   'field.acquisitions_common.acquisitionMethod.name': 'Received as',
   'field.acquisitions_common.acquisitionProvisos.name': 'Restrictions',
