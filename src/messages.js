96241014321532a1a4d3ad593a89b1b7931a13d6
Ray Lee <ray.lee@lyrasis.org>
2020-01-11 13:02:56 -0800

    DRYD-796: Update additional messages for searching on all fields.

--- src/messages.js
+++ src/messages.js
@@ -1,70 +1,40 @@
 export default {
-  'about.title': 'Welcome to the CollectionSpace Local History and Material Culture Demo',
-  'about.contentHTML': `
-    <p>
-      CollectionSpace is a free, open-source collections management application for museums,
-      historical societies, natural science collections, and more.
-    </p>
-    <p>
-      This demo site is running the
-      <a href="http://www.collectionspace.org/current_release/">current version</a> of the
-      <a href="https://collectionspace.atlassian.net/wiki/x/BYA_K">local history and material culture profile</a> of
-      CollectionSpace, which includes fields and procedures useful for local history collections. If
-      you’re interested in a version of CollectionSpace configured to meet the needs of a different
-      domain, please visit our <a href="https://www.collectionspace.org/demo/">demo landing page</a>
-      to view all available options. Examples include Anthropology, Botanical Garden, Fine Art,
-      Public Art, and Herbarium.
-    </p>
-    <p>
-      To log in, use the email <strong>admin@lhmc.collectionspace.org</strong>, with the
-      case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
-      email <strong>reader@lhmc.collectionspace.org</strong> with the password
-      <strong>reader</strong>.
-    </p>
-    <p>
-      Not sure where to get started? Follow along with one of our quick-start guides:
-    </p>
-    <ul>
-      <li>
-        <span>Create a new object:</span>
-        <a href="http://bit.ly/newobjpdf">PDF</a>
-        <a href="https://vimeo.com/112212895">Screencast</a>
-      </li>
-      <li>
-        <span>Link to an image:</span>
-        <a href="http://bit.ly/linkimgpdf">PDF</a>
-        <a href="https://vimeo.com/112214418">Screencast</a>
-      </li>
-      <li>
-        <span>Add a storage location:</span>
-        <a href="http://bit.ly/storlocpdf">PDF</a>
-        <a href="https://vimeo.com/112818120">Screencast</a>
-      </li>
-    </ul>
-    <p>
-      We rebuild this demo site weekly so don't worry, you won't break it!
-    </p>
-  `,
+  'about.title': 'Welcome to CollectionSpace: Local History and Material Culture',
+
+  // acquisition
 
   'field.acquisitions_common.accessionDateGroup.name': 'Accessioned',
   'field.acquisitions_common.acquisitionMethod.name': 'Received as',
   'field.acquisitions_common.acquisitionProvisos.name': 'Restrictions',
   'field.acquisitions_common.acquisitionReferenceNumber.name': 'Accession number',
 
+  // collectionobject
+
+  'field.collectionobjects_common.contentDescription.fullName': 'Content description/summary/scope',
   'field.collectionobjects_common.contentDescription.name': 'Description/summary/scope',
+  'field.collectionobjects_common.contentNote.fullName': 'Content abstract',
   'field.collectionobjects_common.contentNote.name': 'Abstract',
+  'field.collectionobjects_common.inscriptionDescriptionInterpretation.fullName': 'Non-textual inscription maker\'s mark note',
   'field.collectionobjects_common.inscriptionDescriptionInterpretation.name': 'Maker\'s mark note',
   'field.collectionobjects_common.objectStatus.name': 'Original/copy',
   'field.collectionobjects_common.objectHistoryNote.name': 'Provenance',
 
+  // conditioncheck
+
   'field.conditionchecks_common.techAssessmentGroup.name': 'Mechanism operation',
+  'field.conditionchecks_common.techAssessment.fullName': 'Mechanism operation description',
+  'field.conditionchecks_common.techAssessmentDate.fullName': 'Mechanism operation date',
+
+  // intake
 
   'field.intakes_common.currentOwner.name': 'Source',
+  'field.intakes_common.despositorsRequirements.fullName': 'Depositor restriction',
   'field.intakes_common.despositorsRequirements.name': 'Restriction',
   'field.intakes_common.entryDate.name': 'Received date',
   'field.intakes_common.entryMethod.name': 'Received as',
   'field.intakes_common.entryNote.name': 'Note',
   'field.intakes_common.returnDate.name': 'Return by',
+  'field.intakes_common.insuranceRenewalDate.fullName': 'Insurance policy expiration date',
   'field.intakes_common.insuranceRenewalDate.name': 'Policy expiration date',
   'field.intakes_common.insurer.name': 'Insurance company',
   'field.intakes_common.currentLocationGroup.name': 'Interim location',
@@ -73,24 +43,35 @@ export default {
   'field.intakes_common.locationDate.name': 'Inventoried date',
   'field.intakes_common.conditionCheckDate.name': 'Condition date',
   'field.intakes_common.conditionCheckNote.name': 'Condition note',
+  'field.intakes_common.conditionCheckerOrAssessor.fullName': 'Condition check by',
   'field.intakes_common.conditionCheckerOrAssessor.name': 'Condition by',
+
+  // movement
 
   'field.movements_common.movementReferenceNumber.name': 'Object ID',
   'field.movements_common.normalLocation.name': 'Home location/building',
   'inputTable.movement.currentLocation': 'Temp location/building',
-  'field.movements_common.currentLocation.name': 'Location/building',
   'field.movements_common.currentLocation.fullName': 'Temp location/building',
+  'field.movements_common.currentLocation.name': 'Location/building',
+  'field.movements_common.currentLocationFitness.fullName': 'Temp location/building fitness',
+  'field.movements_common.currentLocationNote.fullName': 'Temp location/building note',
   'field.movements_common.locationDate.name': 'Inventoried date',
   'field.movements_common.reasonForMove.name': 'Reason',
-  'field.movements_common.movementContact.name': 'Moved by',
   'field.movements_common.movementContact.fullName': 'Moved by',
+  'field.movements_common.movementContact.name': 'Moved by',
   'field.movements_common.plannedRemovalDate.name': 'Temp location until',
   'field.movements_common.removalDate.name': 'Temp location date',
+  'field.movements_common.movementNote.fullName': 'Temp location note',
   'field.movements_common.movementNote.name': 'Temp location note',
 
+  // person
+
   'field.persons_common.personTermGroupList.required': 'At least one full name is required. Please enter a value.',
+  'field.persons_common.termDisplayName.fullName': 'Term full name',
   'field.persons_common.termDisplayName.name': 'Full name',
+  'field.persons_common.forename.fullName': 'Term first name',
   'field.persons_common.forename.name': 'First name',
+  'field.persons_common.surname.fullName': 'Term last name',
   'field.persons_common.surname.name': 'Last name',
   'field.persons_common.birthDateGroup.name': 'Born',
   'field.persons_common.birthPlace.name': 'Where',
@@ -98,6 +79,8 @@ export default {
   'field.persons_common.deathPlace.name': 'Where',
   'field.persons_common.occupation.name': 'Occupation/sphere of activity',
   'field.persons_common.bioNote.name': 'Note',
+
+  // place
 
   'field.places_common.placeNote.name': 'Legal description',
 };
