d823d49591a94841cc85d9a1ba201d2a25e9309b
Michael Ritter <emericaskate15@gmail.com>
2022-02-10 14:57:32 -0700

    DRYD-1026: Add public browser template

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -1,7 +1,9 @@
 import defaultForm from './default';
 import photoForm from './photo';
+import publicForm from './public';
 
 export default (configContext) => ({
   default: defaultForm(configContext),
   photo: photoForm(configContext),
+  public: publicForm(configContext),
 });
