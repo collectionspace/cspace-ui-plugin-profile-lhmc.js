bf03fbb1cef1d898dab47e368da24dbed2289b70
Michael Ritter <emericaskate15@gmail.com>
2023-05-31 16:20:15 -0600

    DRYD-1213: Add descriptionLevel

--- src/plugins/recordTypes/collectionobject/forms/default.jsx
+++ src/plugins/recordTypes/collectionobject/forms/default.jsx
@@ -48,6 +48,7 @@ const template = (configContext) => {
               </Col>
             </Row>
 
+            <Field name="descriptionLevel" />
             <Field name="recordStatus" />
 
             <Field name="publishToList">
