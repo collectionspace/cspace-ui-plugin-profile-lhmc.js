ec7ceb016250dff6dd39377fdd537b7d1de8fc3d
Michael Ritter <emericaskate15@gmail.com>
2023-06-23 15:30:26 -0600

    DRYD-1128, DRYD-1167: Add controlled object name and material

--- src/plugins/recordTypes/collectionobject/forms/default.jsx
+++ src/plugins/recordTypes/collectionobject/forms/default.jsx
@@ -111,6 +111,7 @@ const template = (configContext) => {
         <Field name="objectNameList">
           <Field name="objectNameGroup">
             <Field name="objectName" />
+            <Field name="objectNameControlled" />
             <Field name="objectNameCurrency" />
             <Field name="objectNameLevel" />
             <Field name="objectNameSystem" />
@@ -164,6 +165,7 @@ const template = (configContext) => {
         <Field name="materialGroupList">
           <Field name="materialGroup">
             <Field name="material" />
+            <Field name="materialControlled" />
             <Field name="materialComponent" />
             <Field name="materialComponentNote" />
             <Field name="materialName" />
