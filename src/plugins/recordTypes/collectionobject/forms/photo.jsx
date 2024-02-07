44b025b6e9af258dd2bc55999a151c5ec7a80453
Michael Ritter <mikejritter@users.noreply.github.com>
2024-02-07 16:37:48 -0700

    DRYD-1374: Add objectCountGroup fields for collectionobject (#14)
    Co-authored-by: Ray Lee <ray.lee@lyrasis.org>

--- src/plugins/recordTypes/collectionobject/forms/photo.jsx
+++ src/plugins/recordTypes/collectionobject/forms/photo.jsx
@@ -23,7 +23,6 @@ const template = (configContext) => {
         <Row>
           <Col>
             <Field name="objectNumber" />
-            <Field name="numberOfObjects" />
 
             <Field name="responsibleDepartments">
               <Field name="responsibleDepartment" />
@@ -82,6 +81,16 @@ const template = (configContext) => {
             <Field name="objectNameType" />
             <Field name="objectNameLanguage" />
             <Field name="objectNameNote" />
+          </Field>
+        </Field>
+
+        <Field name="objectCountGroupList">
+          <Field name="objectCountGroup">
+            <Field name="objectCount" />
+            <Field name="objectCountType" />
+            <Field name="objectCountCountedBy" />
+            <Field name="objectCountDate" />
+            <Field name="objectCountNote" />
           </Field>
         </Field>
       </Panel>
