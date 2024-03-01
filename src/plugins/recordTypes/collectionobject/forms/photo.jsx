e1ecfd469ed7340006f05d2ffc373a119aa3cdde
Michael Ritter <emericaskate15@gmail.com>
2024-03-01 16:08:37 -0700

    DRYD-1397: Add controlled object and material and fix ordering

--- src/plugins/recordTypes/collectionobject/forms/photo.jsx
+++ src/plugins/recordTypes/collectionobject/forms/photo.jsx
@@ -74,6 +74,7 @@ const template = (configContext) => {
 
         <Field name="objectNameList">
           <Field name="objectNameGroup">
+            <Field name="objectNameControlled" />
             <Field name="objectName" />
             <Field name="objectNameCurrency" />
             <Field name="objectNameLevel" />
@@ -112,6 +113,7 @@ const template = (configContext) => {
 
         <Field name="materialGroupList">
           <Field name="materialGroup">
+            <Field name="materialControlled" />
             <Field name="material" />
             <Field name="materialComponent" />
             <Field name="materialComponentNote" />
