2f252dba892166eb46932767d284a1f985b2b653
Michael Ritter <mikejritter@users.noreply.github.com>
2025-05-15 14:12:01 -0600

    DRYD-1770: Add production agent verbatim (#27)

--- src/plugins/recordTypes/collectionobject/forms/default.jsx
+++ src/plugins/recordTypes/collectionobject/forms/default.jsx
@@ -387,6 +387,10 @@ const template = (configContext) => {
               </Field>
             </Field>
 
+            <Field name="objectProductionAgents">
+              <Field name="objectProductionAgent" />
+            </Field>
+
             <Field name="objectProductionNote" />
           </Col>
         </Row>
